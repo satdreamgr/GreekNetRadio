4476a490c6b929a9cff4f88c8c18976bc1f661b4
nautilus7 <spyros.n7@gmail.com>
2019-12-31 19:35:50 +0200

    Updated translations setup

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
+import setup_translate
 
 pkg = 'Extensions.GreekNetRadio'
 setup (name = 'enigma2-plugin-extensions-greeknetradio',
-       version = '3.0',
+       version = '3.1',
        description = 'Internet radio by SatDreamGr',
        package_dir = {pkg: 'plugin'},
        packages = [pkg],
-       package_data = {pkg: ['*stations', 'flex.sh', 'po/el/LC_MESSAGES/lang.*', 'greekradio.png']},
+       package_data = {pkg: ['*stations', 'flex.sh', 'greekradio.png']},
+       cmdclass = setup_translate.cmdclass,
 )
