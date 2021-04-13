dab3d8ec1b050a6d369f9069c131e80fcf967dd6
persianpros <persianpros@yahoo.com>
2021-04-13 22:59:18 +0430

    PEP8 double aggressive E251 and E252

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 import setup_translate
 
 pkg = 'Extensions.GreekNetRadio'
-setup (name = 'enigma2-plugin-extensions-greeknetradio',
-       version = '3.1',
-       description = 'Internet radio by SatDreamGr',
-       package_dir = {pkg: 'plugin'},
-       packages = [pkg],
-       package_data = {pkg: ['*stations', 'greekradio.png']},
-       cmdclass = setup_translate.cmdclass,
+setup (name='enigma2-plugin-extensions-greeknetradio',
+       version='3.1',
+       description='Internet radio by SatDreamGr',
+       package_dir={pkg: 'plugin'},
+       packages=[pkg],
+       package_data={pkg: ['*stations', 'greekradio.png']},
+       cmdclass=setup_translate.cmdclass,
 )
