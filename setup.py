06eb5d273869171ed2799afd722046c62b4f5c87
persianpros <persianpros@yahoo.com>
2021-04-13 22:59:19 +0430

    PEP8 double aggressive E20 and E211

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import setup_translate
 
 pkg = 'Extensions.GreekNetRadio'
-setup (name='enigma2-plugin-extensions-greeknetradio',
+setup(name='enigma2-plugin-extensions-greeknetradio',
        version='3.1',
        description='Internet radio by SatDreamGr',
        package_dir={pkg: 'plugin'},
