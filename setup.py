124b9b1bd4fc67c34452ee07d4c9f3dd7b382e21
Athanasios Oikonomou <athoik@gmail.com>
2022-01-19 22:02:36 +0200

    hardknott: Fix translation

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='enigma2-plugin-extensions-greeknetradio',
        description='Internet radio by SatDreamGr',
        package_dir={pkg: 'plugin'},
        packages=[pkg],
-       package_data={pkg: ['*stations', 'greekradio.png']},
+       package_data={pkg: ['*stations', 'greekradio.png', 'locale/*/LC_MESSAGES/GreekNetRadio.mo']},
        cmdclass=setup_translate.cmdclass,
 )
